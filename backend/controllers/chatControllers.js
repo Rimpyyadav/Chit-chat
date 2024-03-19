537dbc1023e6ba93b803f9d22a4ba8e41705bc83
Rimpy <yadavrimpy1624@gmail.com>
2024-03-19 17:43:37 +0530

    progress

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -43,13 +43,86 @@ const accessChat = expressAsyncHandler(async(req,res)=>{
 }
 });
 
-const fetchChats = asyncHandler(async(res,res) => {
+const fetchChats = asyncHandler(async(req,res) => {
     try{
-        Chat.find({ users: { $elemMatch: {$eq: req.user._id}} }).then((result)=>res.send(result))
+        Chat.find({ users: { $elemMatch: {$eq: req.user._id}} })
+            .populate("users", "-password")
+            .populate("groupAdmin", "-password")
+            .populate("latestMessage")
+                .sort({ upadteAt: -1})
+            .then(async(results) => {
+                results = await User.populate(isChat, {
+                    path: "latestMessage.sender",
+                    select: "name pic email",
+                });
+
+                res.status(200).send(results);
+            });
+
+
 
     }catch(error){
-
+        res.status(400);
+        throw new Error(error.message);
     }
+});
+
+const createGroupChat = asyncHandler(async(req,res) =>{
+    if(!req.body.users || !req.body.name) {
+        return res.status(400).send({ message:"please fill all the fields"})
+    }
+    var users = Json.parse(req.body.users);
+    if(users.length < 2){
+        return res
+        .status(400)
+        .send("More thsn 2 users are required to form a group chat")
+    }
+
+    users.push(req.user);
+
+    try{
+        const groupChat = await Chat.create({
+
+            chatName: req.body.name,
+            users:users,
+            isGroupChat:true,
+            groupAdmin:req.user,
+        });
+
+        const fullGroupChat = await Chat.findOne({_id: groupChat._id})
+        .populate("users", "-password")
+        .populate("groupAdmin", "-password");
+
+        res.status(200).json(fullGroupChat)
+    } catch (error) {
+        res.status(400);
+        throw new Error(error.message);
+    }
+
+});
+
+const  renameGroup = asyncHandler(async(req,res)=> {
+    const {chatId, chatName} = req.body;
+
+    const updatedChat = await Chat.findByIdAndUpdate(
+        chatId,
+        {
+            chatName,
+        },
+        {
+            new: true,
+        }
+    )
+    .populate("users", "-password")
+    .populate("groupAdmin","-password");
+
+    if(!updatedChat){ 
+        res.status(404);
+        throw new Error("chat not found");
+    }else{
+        res.json(updatedChat);
+    }
+
 })
 
-module.exports = { accessChat };
+module.exports = { accessChat, fetchChats, createGroupChat, renameGroup };
