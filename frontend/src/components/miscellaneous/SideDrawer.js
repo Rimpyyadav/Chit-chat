01dc1f7becc3d949859312ddf36161682c69b766
Rimpy <yadavrimpy1624@gmail.com>
2024-03-25 22:37:03 +0530

    progress

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
-import { Box, Tooltip, Button, Text } from '@chakra-ui/react';
+import { Box, Tooltip,Text, MenuButton, MenuItem } from '@chakra-ui/react';
+import {Button} from "@chakra-ui/button";
+import {Avatar} from "@chakra-ui/avatar"
+import { BellIcon, ChevronDownIcon} from "@chakra-ui/icons";
+
+
 
 const SideDrawer = () => {
     const [search, setSearch] = useState("");
     const [searchResult, setSearchResult] = useState([]);
     const [loading, setLoading] = useState(false);
     const [loadingChat, setLoadingChat] = useState();
+
+     
 
     return (
         <>
@@ -18,6 +25,29 @@ const SideDrawer = () => {
                         </Text>
                     </Button>
                 </Tooltip>
+
+                <Text fontSize="2xl" fontFamily="Work-sans" >
+                    Talk-A-Tive
+
+                </Text>
+
+                <div>
+                    <Menu>
+                        <MenuButton p={1} >
+                            <BellIcon fontSize="2xl" m={1} />
+
+                        </MenuButton>
+                    </Menu>
+                    <Menu>
+                        <MenuButton as={Button} rightIcon={<ChevronDownIcon/>} >
+                            <Avatar size="sm" cursor='pointer' name={user.name} />
+
+                        </MenuButton>
+                        <MenuList>
+                            <MenuItem></MenuItem>
+                        </MenuList>
+                    </Menu>
+                </div>
             </Box>
         </>
     );
