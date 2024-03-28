2a70c47ac776a7a03950de9a8957f38aceb60e64
Rimpy <yadavrimpy1624@gmail.com>
2024-03-28 23:47:13 +0530

    progress

--- frontend/src/components/MyChat.js
+++ frontend/src/components/MyChat.js
@@ -1,7 +1,15 @@
 import React from "react";
+import React, {useState} from "react";
+import { ChatState } from "../Context/ChatProvider";
 
 const MyChats = () => {
+    const [loggedUser, setLoggedUser] = useState(); 
     const { selectedChat,setSelectedChat, user, chats, setChats  } = ChatState();
+
+    const toast = useToast({
+        title: "Error Occured",
+        description: "Failed to Load the chats",
+    });
 
     return <div>My Chats</div>
 }
